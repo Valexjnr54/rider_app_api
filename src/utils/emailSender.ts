0ad7bdf5b5b8fa55f4a3ab0eb3c58a96ff49370f
Valexjnr54 <jimvalex54@gmail.com>
2023-12-07 18:20:08 +0100

    new commit

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -6,7 +6,7 @@ import fs from "fs";
 
 const transporter = nodemailer.createTransport({
   host: process.env.Mail_HOST,
-  port: 465,
+  port: 2525,
   auth: {
     user: process.env.Mail_USER,
     pass: process.env.Mail_PASSWORD
